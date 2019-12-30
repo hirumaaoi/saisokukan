910a56cd88a174de26dfb94d81af98cb28e61a38
Aoihiruma <yusandaz.sora.aquarius@gmail.com>
2019-12-30 19:00:00 +0900

    いったん移植

---
+++ 2016/waves2.js
@@ -0,0 +1,164 @@
+///<reference path="../libs/easeljs/easeljs.d.ts" />
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var project;
+(function (project) {
+    "use strict";
+    /** Processing のパーリンノイズ(連続した乱数)関数へのショートカットです。 */
+    var noise;
+    /**
+     * ウェーブ風グラフィックの表示クラスです。
+     * @author Yasunobu Ikeda a.k.a clockmaker
+     * @see FrocessingSample by nutsu http://wonderfl.net/c/kvXp
+     */
+    var WaveShape = (function (_super) {
+        __extends(WaveShape, _super);
+        /**
+         * コンストラクターです。
+         * @param maxLines  線自体の個数です。
+         * @param maxVertex 線の水平方向の頂点数です。
+         * @param debugMode デバッグモードとして実行するかの設定です。trueの場合、デバッグ表示が有効になります。
+         */
+        function WaveShape(maxLines, maxVertex, debugMode) {
+            if (maxLines === void 0) { maxLines = 10; }
+            if (maxVertex === void 0) { maxVertex = 10; }
+            if (debugMode === void 0) { debugMode = false; }
+            _super.call(this);
+            /** 時間経過を示す媒介変数です。 */
+            this._time = 0;
+            // やむを得ない超残念実装
+            noise = new Processing().noise;
+            this._maxLines = maxLines;
+            this._maxVertex = maxVertex;
+            this._debugMode = debugMode;
+            this._vertexArr = [];
+            // Yの頂点座標の初期値を設定
+            for (var i = 0; i < this._maxLines; i++) {
+                this._vertexArr[i] = [];
+                // 頂点座標の上限値はランダムで
+                var num = (this._maxVertex - 1) * Math.random() * Math.random() + 1;
+                // デバッグ機能が有効の場合は
+                if (this._debugMode == true) {
+                    // 頂点数は引数で設定したものと同じ値に設定する
+                    num = this._maxVertex;
+                }
+                for (var j = 0; j <= num; j++) {
+                    // 初期値は全て0で。
+                    this._vertexArr[i][j] = 0;
+                }
+            }
+            this.on("tick", this.handleTick, this);
+        }
+        /**
+         * エンターフレームイベント
+         * @param event
+         */
+        WaveShape.prototype.handleTick = function (event) {
+            // 媒介変数を更新
+            this._time += 0.01;
+            // グラフィックをクリア
+            this.graphics.clear();
+            // 曲線を描き直す
+            for (var i = 0; i < this._maxLines; i++) {
+                var lineWidth = (0.05 * i) + 0.10; // ゼロ対策(ゼロのときに太さが1pxになるため)
+                // デバッグ機能が有効の場合は
+                if (this._debugMode == true) {
+                    // 線を2pxで描く
+                    lineWidth = 1.0;
+                }
+                this.drawWave(this._vertexArr[i], lineWidth, i * 0.10);
+            }
+        };
+        /**
+         * ウェーブを描きます。
+         * @param vertexArr    頂点配列
+         * @param strokeSize    線の太さ
+         * @param timeOffset    波のオフセット
+         */
+        WaveShape.prototype.drawWave = function (vertexArr, strokeSize, timeOffset) {
+            var vertexNum = vertexArr.length - 1;
+            var stageW = window.innerWidth;
+            var stageH = window.innerHeight;
+            // 線のスタイルを設定
+            this.graphics
+                .setStrokeStyle(strokeSize)
+                .beginStroke("white");
+            // 波の次の目標値を計算
+            for (var i = 0; i <= vertexNum; i++) {
+                // 乱数を取得、-0.5〜+0.5の範囲
+                var noiseNum = noise(i * 0.2, this._time + timeOffset) - 0.5;
+                // 目標座標を計算。画面の高さに比例
+                var targetY = noiseNum * stageH * 2;
+                // イージングの公式を使って、頂点座標をなめらかに変化させる
+                vertexArr[i] += (targetY - vertexArr[i]) * 0.05;
+            }
+            // 曲線を描くためにXY座標を計算
+            var BASE_Y = stageH / 2; // 基準は画面の中央のY座標
+            var points = [];
+            // 画面左端の座標
+            points.push({ x: -200, y: BASE_Y });
+            for (var i = 0; i <= vertexNum; i++) {
+                // 途中座標
+                points.push({
+                    x: (stageW * (i / vertexNum)) >> 0,
+                    y: vertexArr[i] + BASE_Y
+                });
+            }
+            // 画面右端の座標
+            points.push({ x: stageW + 200, y: BASE_Y });
+            // 直線情報を曲線にするテクニック
+            // 参考 : http://jsdo.it/clockmaker/createjs-curveto
+            for (var i = 0; i < points.length; i++) {
+                if (i >= 2) {
+                    // マウスの軌跡を変数に保存
+                    var p0x = points[i - 0].x;
+                    var p0y = points[i - 0].y;
+                    var p1x = points[i - 1].x;
+                    var p1y = points[i - 1].y;
+                    var p2x = points[i - 2].x;
+                    var p2y = points[i - 2].y;
+                    // カーブ用の頂点を割り出す
+                    var curveStartX = (p2x + p1x) / 2;
+                    var curveStartY = (p2y + p1y) / 2;
+                    var curveEndX = (p0x + p1x) / 2;
+                    var curveEndY = (p0y + p1y) / 2;
+                    // カーブは中間点を結ぶ。マウスの座標は制御点として扱う。
+                    this.graphics
+                        .moveTo(curveStartX, curveStartY)
+                        .curveTo(p1x, p1y, curveEndX, curveEndY);
+                }
+            }
+            this.graphics.endStroke();
+            // デバッグ機能
+            // 曲線のもとになっている頂点を可視化
+            if (this._debugMode == true) {
+                for (var i = 0; i < points.length; i++) {
+                    // マウスの軌跡を変数に保存
+                    var p0x = points[i - 0].x;
+                    var p0y = points[i - 0].y;
+                    if (i > 0) {
+                        var p1x = points[i - 1].x;
+                        var p1y = points[i - 1].y;
+                        // 線を描く
+                        this.graphics
+                            .setStrokeStyle(0.5)
+                            .beginStroke("red")
+                            .moveTo(p1x, p1y)
+                            .lineTo(p0x, p0y)
+                            .endStroke();
+                    }
+                    // 点をプロットする
+                    this.graphics
+                        .beginFill("red")
+                        .drawCircle(p0x, p0y, 3)
+                        .endFill();
+                }
+            }
+        };
+        return WaveShape;
+    })(createjs.Shape);
+    project.WaveShape = WaveShape;
+})(project || (project = {}));
---
+++ 2017/script.js
@@ -0,0 +1,27 @@
+var fadeDelay,scrollTop;
+
+$(window).on("load", function(){topPage(2000); });
+
+function topPage(fadeDelay){
+
+$(window).on("scroll",function(){
+  scrollTop = $(window).scrollTop();
+if(scrollTop > 30){  $("footer").addClass("bgWhite"); $(".crossfade,.ttl_wrap").addClass("fadeOut");}
+else {$("footer").removeClass("bgWhite"); $(".crossfade,.ttl_wrap").removeClass("fadeOut");}
+});
+
+  setTimeout(function(){
+  $("#canvasOverlay").animate({opacity:0},fadeDelay,function(){$(this).remove()});
+		$("#myCanvas").delay(fadeDelay).animate({opacity:0},function(){$(this).remove()});
+	$(".svg_logo,#shiro").addClass("stopTop");
+	$("#contents").addClass("bottomScroll").removeClass("bgw");
+},1500);
+}
+
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+//
+// ga('create', 'UA-62035241-1', 'auto');
+// ga('send', 'pageview');
